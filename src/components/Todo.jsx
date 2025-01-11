0dafb7263836fa11a24f303ab5fcb39c1002acda
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-11 17:36:49 -0500

    Add initial structure in Todo with input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+
+import "../styles/todo.css";
 
 const Todo = () => {
-  return (
-    <div>Todo</div>
-  )
-}
+  const [todo, setTodo] = useState("");
 
-export default Todo
+  return (
+    <>
+      <div className="container">
+        <h2>Todo</h2>
+      </div>
+      <div className="input-container">
+      <div class="input-textContainer">
+        <input type="text" class="text-input" placeholder='Wanna do something?' value={todo} onChange={e => setTodo(e.target.value)} />
+        <div class="input-line"></div>
+      </div>
+      <button className="addButton">Add</button>
+      </div>
+     
+
+      <div className="status-buttons">
+        <button>All</button>
+        <button>Pending!</button>
+        <button>Completed</button>
+      </div>
+      <TodoList />
+    </>
+  );
+};
+
+export default Todo;
