dd754d007ccaaff207d235ddec52b6b6ff7001f7
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-11 18:44:41 -0500

    Test the add Todo logic by rendering in TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useTodoContext } from '../context/TodoContext'
 
 const TodoList = () => {
+  const {state} = useTodoContext()
   return (
-    <div>TodoList</div>
+    <div>{state.todos.map(todo => (
+      <li key={todo.id}>{todo.text.text}</li>
+    ))}</div>
   )
 }
 
 export default TodoList
+
