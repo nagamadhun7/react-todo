2f245fd2ee0f957aaeb11c4c44f078a35d112919
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-11 18:25:15 -0500

    Add logic to add Todo using dispatch

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,9 +2,25 @@ import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 import "../styles/todo.css";
+import { useTodoContext } from "../context/TodoContext";
 
 const Todo = () => {
   const [todo, setTodo] = useState("");
+
+  const {state, dispatch} = useTodoContext()
+
+  const handleAddTodo = () => {
+    if (!todo.trim()) {
+      alert("Please enter a valid todo!");
+      return;
+    }
+    const newTodo = {
+      text: todo,
+    }
+
+    dispatch({type:'addTodo', payload: newTodo})
+    setTodo('')
+  }
 
   return (
     <>
@@ -16,7 +32,7 @@ const Todo = () => {
         <input type="text" className="text-input" placeholder='Wanna do something?' value={todo} onChange={e => setTodo(e.target.value)} />
         <div className="input-line"></div>
       </div>
-      <button className="addButton">Add</button>
+      <button className="addButton" onClick={handleAddTodo}>Add</button>
       </div>
      
 
