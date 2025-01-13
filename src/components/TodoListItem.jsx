c31a0f596068d60088f424c3753e37eb99044065
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-13 07:24:51 -0500

    Add toggle and delete buttons in TodoListItem

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "../styles/todoListItem.css";
 
-const TodoListItem = () => {
+
+const TodoListItem = ({todo}) => {
   return (
-    <div>TodoListItem</div>
+    <div className='todo-container'>
+      <button className='toggle-toDo'>✔️</button>
+      <p>{todo.text.text}</p>
+      <button className='deleteTodo'>❌</button>
+    </div>
   )
 }
 
