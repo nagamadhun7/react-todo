5b5ff3b26445bd4356a025b2509984383fe5cf73
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-13 07:50:17 -0500

    Add conditional styling for the toggle button

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import "../styles/todoListItem.css";
+import { useTodoContext } from '../context/TodoContext';
 
 
 const TodoListItem = ({todo}) => {
+
+  const {dispatch} = useTodoContext()
+
+  const toggleTodo = () => {
+    dispatch({type:'toggleTodo', payload: todo.id})
+  }
   return (
-    <div className='todo-container'>
-      <button className='toggle-toDo'>✔️</button>
+    <div className={`todo-container ${todo.isDone? 'todo-completed' : ''}`}>
+      <button className='toggle-toDo' onClick={toggleTodo}>{todo.isDone ? '✅' : '✔️'}</button>
       <p>{todo.text.text}</p>
       <button className='deleteTodo'>❌</button>
     </div>
