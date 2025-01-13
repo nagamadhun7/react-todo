99597ac0ec4879382479f7520d2279e8e2703372
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-13 08:09:58 -0500

    Add animation for delete todo in TodoListItem

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/todoListItem.css";
 import { useTodoContext } from '../context/TodoContext';
 
@@ -7,15 +7,20 @@ const TodoListItem = ({todo}) => {
 
   const {dispatch} = useTodoContext()
 
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const toggleTodo = () => {
     dispatch({type:'toggleTodo', payload: todo.id})
   }
 
   const deleteTodo = () => {
-    dispatch({type: 'deleteTodo', payload: todo.id})
+    setIsDeleting(true);
+    setTimeout(() => {
+      dispatch({type: 'deleteTodo', payload: todo.id})
+    }, 500);
   }
   return (
-    <div className={`todo-container ${todo.isDone? 'todo-completed' : ''}`}>
+    <div className={`todo-container ${todo.isDone? 'todo-completed' : ''} ${isDeleting ? "deleting" : ""}`}>
       <button className='toggle-toDo' onClick={toggleTodo}>{todo.isDone ? '✅' : '✔️'}</button>
       <p>{todo.text.text}</p>
       <button className='deleteTodo' onClick={deleteTodo}>❌</button>
