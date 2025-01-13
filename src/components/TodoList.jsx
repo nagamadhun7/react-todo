68f3f13bee55d4fb678f12b6340b0c3d0dd7aec6
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-13 08:24:04 -0500

    Testing the filter logic with initial setup

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,12 +6,26 @@ import TodoListItem from './TodoListItem'
 
 const TodoList = () => {
   const {state} = useTodoContext()
+
+  const filterStatus = (status) => {
+    switch (status) {
+      case 'all':
+         console.log(state.todos)
+        break;
+      case 'pending':
+        console.log(state.todos.filter(todo => todo.isDone === false))
+      case 'completed':
+        console.log(state.todos.filter(todo => todo.isDone === true))
+      default:
+        break;
+    }
+  }
   return (
     <>
      <div className="status-buttons">
-        <button>All</button>
-        <button>Pending!</button>
-        <button>Completed</button>
+        <button onClick={() => filterStatus('all')}>All</button>
+        <button onClick={() => filterStatus('pending')}>Pending!</button>
+        <button onClick={() => filterStatus('completed')}>Completed</button>
       </div>
       <div>{state.todos.map(todo => (
       <TodoListItem key={todo.id} todo={todo} />
